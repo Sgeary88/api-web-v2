a074b8481b65ecd61b0040499b350d54d5684abb
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2024-12-02 17:02:12 -0800

    added add/remove favorites button, not seeing favorite section

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const imagePromises = [];
 const favorites = [];
 let dogImages = [];
+let favoriteImages = [];
 
 for (let i = 0; i < 30; i++) {
   imagePromises.push(fetch("https://dog.ceo/api/breeds/image/random"));
@@ -19,6 +20,7 @@ Promise.all(imagePromises)
   })
   .then((dataArray) => {
     const dogImagesContainer = document.getElementById("dog-images-container");
+    const favoritesContainer = document.getElementById("favorites-container");
 
     // Create cards for images
     dataArray.forEach((data) => {
@@ -29,10 +31,42 @@ Promise.all(imagePromises)
       imgElement.src = data.message;
       imgElement.alt = "Picture of a dog";
 
+      const addToFavoritesButton = document.createElement("button");
+      addToFavoritesButton.textContent = "Add to Favorites";
+      addToFavoritesButton.addEventListener("click", () => {
+        addToFavorites(data, dogCard);
+      });
+
       dogCard.appendChild(imgElement);
+      dogCard.appendChild(addToFavoritesButton);
+
       dogImages.push(dogCard);
       dogImagesContainer.appendChild(dogCard);
     });
+
+    // function to add item to favorites
+    function addToFavorites(data, dogCard) {
+      dogImagesContainer.removeChild(dogCard);
+      favorites.push({ data });
+
+      const favoriteCard = document.createElement("div");
+      favoriteCard.className = "dog-card";
+
+      const imgElement = document.createElement("img");
+      imgElement.src = data.message;
+      imgElement.alt = "Dog Image";
+
+      const removeFromFavoritesButton = document.createElement("button");
+      removeFromFavoritesButton.textContent("Remove from Favorites");
+      removeFromFavoritesButton.addEventListener("click", () => {
+        removeFromFavoritesButton(data, favoriteCard);
+      });
+      favoriteCard.appendChild(imgElement);
+      favoriteCard.appendChild(removeFromFavoritesButton);
+
+      favoriteImages.push(favoriteCard);
+      favoritesContainer.appendChild(favoriteCard);
+    }
   })
   .catch((err) => {
     console.error("Error fetching images", err);
